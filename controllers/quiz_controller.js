073c7e26b26359c93b9ee20c39d1ef31b9c0773a
Alberto Díaz <dmalber@gmail.com>
2015-07-27 23:41:26 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,6 +1,21 @@
 var models = require('../models/models.js');
 
 
+// Autoload - factorización de código
+exports.load = function(req, res, next, quizId) {
+    models.Quiz.find(quizId).then(
+        function(quiz) {
+            if (quiz) {
+                req.quiz = quiz;
+                next();
+            }
+            else {
+                next(new Error('No existe quiz: ' + quizId));
+            }
+        }
+    ).catch(function(error){next(error);});
+}
+
 // GET /author
 exports.author = function(req, res) {
     res.render('author', {author: 'Alberto Díaz Martínez'});
@@ -17,18 +32,18 @@ exports.index = function(req, res) {
 // GET /quizes/:id
 exports.show = function(req, res) {
     models.Quiz.find(req.params.quizId).then(function(quiz) {
-        res.render('quizes/show', {quiz: quiz});
+        res.render('quizes/show', {quiz: req.quiz});
     }); 
 }
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
     models.Quiz.find(req.params.quizId).then(function(quiz) {    
-        if (req.query.respuesta.toUpperCase() === quiz.respuesta.toUpperCase()) {
-            res.render('quizes/answer', {quiz: quiz, respuesta: '¡Correcto!'});
+        if (req.query.respuesta.toUpperCase() === req.quiz.respuesta.toUpperCase()) {
+            res.render('quizes/answer', {quiz: req.quiz, respuesta: '¡Correcto!'});
         }
         else {
-            res.render('quizes/answer', {quiz: quiz, respuesta: '¡Incorrecto!'});
+            res.render('quizes/answer', {quiz: req.quiz, respuesta: '¡Incorrecto!'});
         }
     });
 }
